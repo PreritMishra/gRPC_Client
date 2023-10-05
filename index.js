ccd17e0e6840fee3bb3476514492b8ce2754e09d
Prerit <preritmishra1002@gmail.com>
2023-10-05 16:21:00 +0530

    Added grpc Client

---
+++ index.js
@@ -0,0 +1,28 @@
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+
+const packageDefinition = protoLoader.loadSync('./todo.proto', {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+});
+
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+var todoService = protoDescriptor.TodoService;
+
+const client = new todoService('localhost:50051', grpc.credentials.createInsecure());
+
+client.listTodos({}, (err,todos) => {
+    if(!err) {
+        console.log(todos);
+        client.createTodo({id: 3, title: 'third todo', content: 'yooooo'}, (err,todo) => {
+            if(!err) {
+                console.log("Created a new todo");
+            } else {
+                console.log(err);
+            }
+        });
+    }
+});
